f23390c05df57ed216e8fdbbea27ebccde1ffb5f
XeroWTF <eugedsr@gmail.com>
2023-05-05 11:36:02 -0300

    retoque

--- rick-y-morty-tp/src/components/Card/Card.jsx
+++ rick-y-morty-tp/src/components/Card/Card.jsx
@@ -1,16 +1,17 @@
 import styles from "./Card.module.css"
 
+
 const Card = ({id, name, status, species, gender, origin, image, onClose}) =>
 {
    return (
       <div className ={styles.contenedor}>
+         <button onClick={onClose} >X</button>
          <h1>{name}</h1>         
          <img src={image} alt='' /> 
          <h2> Species: {species}</h2>
          <h2>Gender: {gender}</h2>
-         <h2>Status: {status}</h2>
-         <h2>Origin: {origin}</h2>
-         <button onClick={onClose}>Funar</button>
+         {/* <h2>Status: {status}</h2>  */}
+         {/* <h2>Origin: {origin}</h2> */}
       </div>
    );
 }
