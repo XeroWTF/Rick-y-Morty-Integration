26984648a4aa58e979bf9777d7f2675621d6b700
XeroWTF <eugedsr@gmail.com>
2023-05-23 20:44:22 -0300

    Api con filtros funcionales

--- rick-y-morty-tp/src/Redux/reducer.js
+++ rick-y-morty-tp/src/Redux/reducer.js
@@ -20,24 +20,28 @@ const initialState = {
           ),
         };
   
-      case "FILTER":
-        const allCharacterFiltered = state.allCharactersFav.filter(
-          (char) => char.gender === action.payload
-        );
-        return {
-          ...state,
-          myFavorites: allCharacterFiltered,
-        };
+        case "FILTER":
+  const allCharacterFiltered = state.allCharactersFav.filter(
+    (char) => char.gender === action.payload
+  );
+  return {
+    ...state,
+    myFavorites: allCharacterFiltered,
+  };
+
+          
   
-      case "ORDER":
-        const allCharactersFavCopy = [...state.allCharactersFav];
-        return {
-          ...state,
-          myFavorites:
-            action.payload === "A"
-              ? allCharactersFavCopy.sort((a, b) => a.id - b.id)
-              : allCharactersFavCopy.sort((a, b) => b.id - a.id),
-        };
+  case "ORDER":
+    const allCharactersFavCopy = [...state.allCharactersFav];
+    const sortedFavorites =
+      action.payload === "A"
+        ? allCharactersFavCopy.sort((a, b) => a.id - b.id)
+        : allCharactersFavCopy.sort((a, b) => b.id - a.id);
+    return {
+      ...state,
+      myFavorites: sortedFavorites,
+    };
+  
   
       default:
         return { ...state };
